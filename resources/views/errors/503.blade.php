389ad7633a540f092a1f3d217c0b9001d802aae3
ThishanthSmakintel <dev15.smakintel@gmail.com>
2024-04-24 06:07:47 +0530

    auto deploy check

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -14,7 +14,7 @@
                             <h2>We'll Be Back Soon!</h2>
                             <p>Our website is currently undergoing scheduled maintenance. We apologize for any inconvenience
                                 this may cause. Please check back later.</p>
-                            <p>Estimated downtime: <strong>2 hours</strong></p>
+                            {{-- <p>Estimated downtime: <strong>2 hours</strong></p> --}}
                         </div>
                     </div>
                 </div>
