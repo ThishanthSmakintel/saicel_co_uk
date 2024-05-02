1a80ba2a0a1b11357e2d59690ab7fdc92795fdeb
ThishanthSmakintel <dev15.smakintel@gmail.com>
2024-05-02 06:38:02 +0530

    triaing page puldated

--- resources/views/training/training.blade.php
+++ resources/views/training/training.blade.php
@@ -32,51 +32,84 @@
         </div>
     </div>
 
-    <section class="card mx-0 shadow">
-        <div class="card-body text-left">
-            <section id="ts-service-area" class="ts-service-area pb-0">
-                <div class="container">
-                    <div class="row text-left align-items-center">
-                        <div class="col-lg-6">
-                            <div class="ts-service-box">
-                                <div class="ts-service-box-info">
-                                    <h2 class="service-box-title fs-5"><strong>Expert Training Services</strong></h2>
-                                    <p>Discover our comprehensive range of <strong>training programs</strong> designed to
-                                        empower your team and enhance their skills:</p>
-                                    <p>We offer <strong>customized training solutions</strong> tailored to your specific
-                                        organizational needs, ensuring <strong>maximum impact</strong> and <strong>lasting
-                                            results</strong>.</p>
-                                    <p>Our services include <strong>flexible scheduling</strong>, <strong>high-quality
-                                            materials</strong>, and <strong>interactive learning experiences</strong> for
-                                        effective training delivery.</p>
-                                    <p>Trust our experienced team to provide <strong>reliable</strong> and
-                                        <strong>engaging</strong> training services that meet your organization's goals.
-                                    </p>
-                                </div>
-                            </div><!-- Service box end -->
-                        </div><!-- Col end -->
-                        <div class="col-lg-6">
-                            <div class="ts-service-box bg-image hover-zoom">
-                                <img loading="lazy" class="img-fluid"
-                                    src="{{ asset('images/training/img-training-card.png') }}" alt="training-service-image">
-                            </div><!-- Service box end -->
-                        </div><!-- Col end -->
-                    </div><!--/ Title row end -->
-                </div><!--/ Container end -->
-            </section>
-        </div>
+
+    <section id="ts-service-area" class="ts-service-area pb-0">
+        <div class="container">
+            <div class="row text-left align-items-center">
+                <div class="col-lg-6">
+                    <div class="ts-service-box">
+                        <div class="ts-service-box-info">
+                            <h2 class="service-box-title fs-5"><strong>Expert Training Services</strong></h2>
+                            <p>Discover our comprehensive range of <strong>training programs</strong> designed to
+                                empower your team and enhance their skills:</p>
+                            <p>We offer <strong>customized training solutions</strong> tailored to your specific
+                                organizational needs, ensuring <strong>maximum impact</strong> and <strong>lasting
+                                    results</strong>.</p>
+                            <p>Our services include <strong>flexible scheduling</strong>, <strong>high-quality
+                                    materials</strong>, and <strong>interactive learning experiences</strong> for
+                                effective training delivery.</p>
+                            <p>Trust our experienced team to provide <strong>reliable</strong> and
+                                <strong>engaging</strong> training services that meet your organization's goals.
+                            </p>
+                        </div>
+                    </div><!-- Service box end -->
+                </div><!-- Col end -->
+                <div class="col-lg-6">
+                    <div class="ts-service-box bg-image hover-zoom">
+                        <img loading="lazy" class="img-fluid" src="{{ asset('images/training/img-training-card.png') }}"
+                            alt="training-service-image">
+                    </div><!-- Service box end -->
+                </div><!-- Col end -->
+            </div><!--/ Title row end -->
+        </div><!--/ Container end -->
     </section>
+
+
+    <section id="ts-service-area card" class="ts-service-area pb-0">
+        <div class="container">
+            <div class="row text-left align-items-center">
+                <div class="col-lg-6">
+                    <div class="ts-service-box">
+                        <div class="ts-service-box-info">
+                            <h2 class="service-box-title fs-5"><strong>Complete the Course and Get
+                                    Certified</strong></h2>
+                            <p>Ready to level up your skills? Complete our courses and earn certification to
+                                showcase your expertise:</p>
+                            <ul class="fa-ul">
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Gain in-depth knowledge in your
+                                    field of interest.</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Receive personalized feedback
+                                    and guidance from industry experts.</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Access to exclusive resources
+                                    and learning materials.</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Network with like-minded
+                                    professionals and expand your career opportunities.</li>
+                            </ul>
+                            <p>Explore our wide range of high-quality courses and enroll today:</p>
+                        </div>
+                    </div><!-- Service box end -->
+                </div><!-- Col end -->
+                <div class="col-lg-6">
+                    <div class="ts-service-box bg-image hover-zoom">
+                        <img loading="lazy" class="img-fluid"
+                            src="{{ asset('images/training/card-img/img-certified.jpg') }}" alt="training-service-image">
+                    </div><!-- Service box end -->
+                </div><!-- Col end -->
+            </div><!--/ Title row end -->
+        </div><!--/ Container end -->
+    </section>
+
+
 
 
     <div class="container my-4">
         <div class="row justify-content-center">
             <div class="col-lg-10">
-                <h1 class="text-center ">Our Awesome Courses</h1>
-                <h4 class="text-center mb-4">Explore our wide range of high-quality courses</h4>
+                <h1 class="text-center mb-4">Checkout Our Awesome Courses</h1>
                 <div class="row row-cols-1 row-cols-lg-3 g-4">
                     @foreach ($courses as $course)
                         <div class="col mb-4">
-                            <div class="card card-hover-effect h-100 d-flex flex-column">
+                            <div class="card card-hover-effect h-100">
                                 <a href="{{ $course['url'] }}">
                                     <img src="{{ asset($course['img']) }}" class="card-img-top"
                                         alt="{{ $course['title'] }}">
@@ -86,7 +119,7 @@
                                     <h5 class="card-title text-center">{{ $course['title'] }}</h5>
                                     <p class="card-category text-center">{{ $course['category'] }}</p>
                                     <p class="card-description text-center">{{ $course['description'] }}</p>
-                                    <a href="{{ $course['url'] }}" class="btn btn-primary mt-auto">Enroll Now</a>
+                                    <a href="{{ $course['url'] }}" class="btn btn-primary btn-enroll">Enroll Now</a>
                                 </div>
                             </div>
                         </div>
@@ -95,14 +128,10 @@
             </div>
         </div>
     </div>
-
-
-
-
 @endsection
+
 <style>
     .card {
-
         transition: transform 0.3s, box-shadow 0.3s;
         border: 1px solid #ddd;
         border-radius: 8px;
@@ -111,7 +140,7 @@
         height: 100%;
     }
 
-    .card-hover-effect:hover {
+    .card:hover {
         transform: scale(1.05);
         box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
     }
